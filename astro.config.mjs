3c82fa93b2666b32a578103d3fde98c1896451c4
Szymon <04_barista_pads@icloud.com>
2025-01-25 12:48:02 +0100

    Add test comment to test commit syncing

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import db from '@astrojs/db';
-import tailwind from '@astrojs/tailwind';
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [db(), tailwind()]
-});
